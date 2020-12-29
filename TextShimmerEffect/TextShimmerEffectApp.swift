5e41aa449ac101ed5006bb0a69a04d8b0100aa14
Max <maxim.macari.dam@gmail.com>
2020-12-29 23:20:12 +0100

    Initial Commit

---
+++ TextShimmerEffect/TextShimmerEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextShimmerEffectApp.swift
+//  TextShimmerEffect
+//
+//  Created by Maxim Macari on 29/12/20.
+//
+
+import SwiftUI
+
+@main
+struct TextShimmerEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
